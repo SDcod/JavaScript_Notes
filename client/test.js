ed7f8e0ecd7b772a1f916cc6f908cdf9b3657e49
Swaroop D <swaroopmdongare@gmail.com>
2024-10-17 23:14:29 +0530

    notes update

--- client/test.js
+++ client/test.js
@@ -1,10 +1,5 @@
-var arr = [1, 2, 3];
-// var arr = {
-//   0: 1,
-//   1: 2,
-//   2: 3,
-// };
+print();
 
-// ** in js even if we add a value at a -ve index it will NOT throw an error
-arr[-1] = 10; //
-console.log(Array.isArray(arr));
+var print = function () {
+  console.log("hello");
+};
