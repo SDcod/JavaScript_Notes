78d9aa7067289bc4745454b8a71ae4f9fa8eb6fb
Swaroop D <swaroopmdongare@gmail.com>
2024-11-10 23:12:48 +0530

    dsa1

--- client/test.js
+++ client/test.js
@@ -1,7 +1,15 @@
-let x = [1, 2];
-var print = function () {
-  x.unshift("asd");
-  console.log(x);
+let nums = [1, 2, 3, 4, 5, 6, 7]; //output [6,5,1,2,3,4]
+
+const reverse = (arr, start, end) => {
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    start++;
+    end--;
+  }
+  return arr;
 };
 
-print();
+console.log(reverse(nums, 0, nums.length - 1));
