4021d4df13f043c891cbb9d834b26e4522c4e3c4
Swaroop D <swaroopmdongare@gmail.com>
2024-12-17 22:54:52 +0530

    hashMapRemoveduplicates

---
+++ DSA-basic/09RemoveDuplicatedFromUnsorted.js
@@ -0,0 +1,26 @@
+//To remove duplicated form an unsorted array.
+let arr = [4, 3, 2, 4, 1, 3, 2, 1];
+
+//using set
+
+// function rmDuplicates(a) {
+//   let uniqueSet = new Set(a);
+
+//   return Array.from(uniqueSet); //Array.from() converts any object with length property to array.
+// }
+// console.log(rmDuplicates(arr));
+
+//Using hashmap**********************************************************8
+const rmDupHash = (a) => {
+  let hashMap = {}; //declare a empty hashMap/Object
+  let result = [];
+
+  for (let num of a) {
+    if (!hashMap[num]) {
+      result.push(num);
+      hashMap[num] = true;
+    }
+  }
+  return result;
+};
+console.log(rmDuplicates(arr));
